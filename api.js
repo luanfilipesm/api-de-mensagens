9599a48c11f62942bdf6f49ae55beb0470a664c2
luanfilipesm <lluuaannfsm@gmail.com>
2022-10-19 16:20:02 -0300

    make it better

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
-const mensagens = require("./src/mensagens.json")
+const mensagens = require("./src/mensagens/mensagens.json")
 
 app.get("/mensage",(req,res)=>{
     return res.json(mensagens)
